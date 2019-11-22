0a8e205cfda8ad169370fd41a4fab07132854a5b
Cherry <yiminxuezheng@gmail.com>
2019-11-22 15:49:43 -0500

    first commit

---
+++ PlatonicRelationships/PlatonicRelationshipsEntry.cs
@@ -0,0 +1,28 @@
+﻿using StardewModdingAPI;
+using Harmony;
+using System.Reflection;
+using StardewValley;
+using StardewValley.Menus;
+
+namespace PlatonicRelationships
+{
+    public class ModEntry : Mod
+    {
+        static internal IMonitor monitor;
+        public override void Entry(IModHelper helper)
+        { 
+            //keep a static version of Monitor so I can make logs from the patch classes
+            monitor = Monitor;
+            var harmony = HarmonyInstance.Create("cherry.platonicrelationships");
+            harmony.Patch(
+                original: AccessTools.Method(typeof(SocialPage), name: "drawNPCSlot"),
+                transpiler: new HarmonyMethod(type: typeof(PatchDrawNPCSlot), nameof(PatchDrawNPCSlot.Transpiler))
+            );
+
+            harmony.Patch(
+                original: AccessTools.Method(typeof(Farmer), name: "changeFriendship"),
+                transpiler: new HarmonyMethod(typeof(PatchChangeFriendship), nameof(PatchChangeFriendship.Transpiler))
+            );
+        }
+    }
+}
