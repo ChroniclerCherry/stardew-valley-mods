38f86b7eb43defdd8df4ae02b08864df330de12c
Cherry <yiminxuezheng@gmail.com>
2019-11-28 22:06:08 -0500

    CustomizeAnywhere initial commit

---
+++ CustomizeAnywhere/CustomizeAnywhereEntry.cs
@@ -0,0 +1,58 @@
+﻿using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewValley;
+using StardewValley.Menus;
+
+namespace CustomizeAnywhere
+{
+    public class ModEntry : Mod
+    {
+        private ModConfig Config;
+
+        public override void Entry(IModHelper helper)
+        {
+            helper.Events.Input.ButtonPressed += this.OnButtonPressed;
+        }
+
+
+        private void OnButtonPressed(object sender, ButtonPressedEventArgs e)
+        {
+
+            // ignore if player hasn't loaded a save yet
+            if (!Context.CanPlayerMove)
+                return;
+
+            this.Config = this.Helper.ReadConfig<ModConfig>();
+            var input = this.Helper.Input;
+            if (input.IsDown(this.Config.ActivateButton)) {
+                if (input.IsDown(this.Config.customizeButton))
+                {
+                    Game1.activeClickableMenu = new CharacterCustomization(CharacterCustomization.Source.Wizard); ;
+                } 
+                else if (input.IsDown(this.Config.changeButton))
+                {
+                    Game1.activeClickableMenu = new CharacterCustomization(CharacterCustomization.Source.Dresser);
+                }
+
+            }
+
+        }//end buttonPressed event
+
+
+    }//end ModEntry
+
+    class ModConfig
+    {
+        public SButton ActivateButton { get; set; }
+        public SButton customizeButton { get; set; }
+        public SButton changeButton { get; set; }
+
+        public ModConfig()
+        {
+            ActivateButton = SButton.LeftShift;
+            customizeButton = SButton.D1;
+            changeButton = SButton.D2;
+        }
+    }
+    
+}
---
+++ ProfitMargins/ProfitMargins.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewModdingAPI.Utilities;
+using StardewValley;
+
+namespace ProfitMargins
+{
+    /// <summary>The mod entry point.</summary>
+    public class ModEntry : Mod
+    {
+        /*********
+        ** Properties
+        *********/
+        /// <summary>The mod configuration from the player.</summary>
+        private ModConfig config;
+        private float originalDifficulty;
+
+        /*********
+        ** Public methods
+        *********/
+
+        public override void Entry(IModHelper helper)
+        {
+            config = helper.ReadConfig<ModConfig>();
+            helper.Events.GameLoop.DayStarted += DayStarted;
+            helper.Events.GameLoop.Saving += this.OnSaving;
+            return;
+
+        }
+
+        /*********
+        ** Private methods
+        *********/
+
+        private void DayStarted(object sender, DayStartedEventArgs args)
+        {
+            if (checkContext()) { 
+                originalDifficulty = Game1.player.difficultyModifier;
+                Game1.player.difficultyModifier = config.ProfitMargin;
+            }
+
+        }
+
+        private void OnSaving(object sender, SavingEventArgs args)
+        {
+            if (checkContext())
+            {
+                Game1.player.difficultyModifier = originalDifficulty;
+                this.Monitor.Log("During save, DL:" + Game1.player.difficultyModifier.ToString(), LogLevel.Debug);
+            }
+
+        }
+
+        private bool checkContext()
+        {
+            if (!Context.IsMainPlayer)
+            {
+                return false;
+            } else if (Context.IsMultiplayer && !config.EnableInMultiplayer)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+
+    class ModConfig
+    {
+        public bool EnableInMultiplayer { get; set; } = false;
+        public float ProfitMargin { get; set; } = 1f;
+    }
+}
