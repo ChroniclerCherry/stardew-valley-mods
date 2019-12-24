6486c98e1796ce78eb534f3313cbec31dc251a6b
Cherry <yiminxuezheng@gmail.com>
2019-12-23 21:24:40 -0500

    Initial commits for CustomShopActionFramework and LimitedCampfireCooking

---
+++ CustomShopActionFramework/ContentModel.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using StardewValley;
+
+namespace CustomShopActionFramework
+{
+    class ContentModel
+    {
+        public ShopPack[] Shops { get; set; }
+    }
+
+    class ShopPack
+    {
+        public string ShopName { get; set; }
+        public string Portrait { get; set; }
+        public string Quote { get; set; }
+        public int ShopPrice { get; set; }
+        public Stock[] Stock { get; set; }
+
+        public ShopPack()
+        {
+            var npc = new NPC
+            {
+                Portrait = ModEntry.helper.Content.Load<Texture2D>(Portrait)
+            };
+            //Game1.removeThisCharacterFromAllLocations(npc);
+        }
+    }
+
+    class Stock
+    {
+        public string ItemType { get; set; }
+        public int StockPrice { get; set; }
+        public string[] Items { get; set; }
+        public string When { get; set; }
+    }
+}
---
+++ LimitedCampfireCooking/ModEntry.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.Xna.Framework;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewValley;
+using StardewValley.Menus;
+using System;
+
+namespace LimitedCampfireCooking
+{
+    class ModEntry : Mod
+    {
+        internal static ModConfig Config;
+        internal static IMonitor monitor;
+        public override void Entry(IModHelper helper)
+        {
+            monitor = Monitor;
+            Config = this.Helper.ReadConfig<ModConfig>();
+            helper.Events.Input.ButtonPressed += Input_ButtonPressed;
+        }
+
+        private bool IsClickWithinReach(Vector2 tile)
+        {
+            var playerPosition = Game1.player.Position;
+            var playerTile = new Vector2(playerPosition.X / 64, playerPosition.Y / 64);
+
+            if (tile.X < (playerTile.X - 1.5) || tile.X > (playerTile.X + 1.5))
+                return false;
+
+            if (tile.Y < (playerTile.Y - 1.5) || tile.Y > (playerTile.Y + 1.5))
+                return false;
+
+            return true;
+        }
+
+        private void Input_ButtonPressed(object sender, ButtonPressedEventArgs e)
+        {
+            if (Context.IsWorldReady &&
+                Game1.currentLocation != null &&
+                Game1.activeClickableMenu == null &&
+                e.Button.IsUseToolButton())
+            {
+                GameLocation loc = Game1.currentLocation;
+                Vector2 tile = e.Cursor.Tile;
+                loc.Objects.TryGetValue(tile, out StardewValley.Object obj);
+
+                if (!IsClickWithinReach(tile))
+                {
+                    return;
+                }
+
+                if (obj != null && obj.Name == "Campfire" && obj.IsOn)
+                {
+                    Vector2 centeringOnScreen = Utility.getTopLeftPositionForCenteringOnScreen(800 + IClickableMenu.borderWidth * 2, 600 + IClickableMenu.borderWidth * 2, 0, 0);
+                    Game1.activeClickableMenu = new CookingMenu((int)centeringOnScreen.X, (int)centeringOnScreen.Y, 800 + IClickableMenu.borderWidth * 2, 600 + IClickableMenu.borderWidth * 2);
+                }
+            }
+        }
+    }
+
+    class ModConfig
+    {
+        public bool EnableAllCookingRecipies { get; set; } = false;
+        public string[] Recipes { get; set; } = {
+                    "Fried Egg",
+                    "Baked Fish",
+                    "Parsnip Soup",
+                    "Vegetable Stew",
+                    "Bean Hotpot",
+                    "Glazed Yams",
+                    "Carp Surprise",
+                    "Fish Taco",
+                    "Tom Kha Soup",
+                    "Trout Soup",
+                    "Pumpkin Soup",
+                    "Algae Soup",
+                    "Pale Broth",
+                    "Roasted Hazelnuts",
+                    "Chowder",
+                    "Lobster Bisque",
+                    "Fish Stew"
+                };
+    }
+}
