fbac6004fcf16818265d622c4dc2e5b7f5bd68de
Cherry <yiminxuezheng@gmail.com>
2020-01-07 16:16:12 -0500

    Adds checks for BFAV in patch

--- ShopTileFramework/Patches.cs
+++ ShopTileFramework/Patches.cs
@@ -7,19 +7,36 @@ namespace ShopTileFramework
     [HarmonyPatch(typeof(StardewValley.Utility), "getPurchaseAnimalStock")]
     internal class PatchGetPurchaseAnimalStock
     {
-        //we want ours to override BFAV's
+        //we want ours to override BFAV's patch
         [HarmonyPriority(Priority.First)]
         public static bool Prefix(ref List<StardewValley.Object> __result)
         {
             ModEntry.monitor.Log("Applying prefix patch to Utility.getPurchaseAnimalStock...", StardewModdingAPI.LogLevel.Debug);
             try
             {
-                // Grab the farm animals for purchase
-                var stock = ModEntry.BFAV.GetAnimalShopStock(Game1.getFarm());
-                var newAnimalStock = new List<StardewValley.Object>();
-                foreach (var animal in stock)
+                //if the exclusion list is empty, fall back to original logic
+                if (ModEntry.ExcludeFromMarnie.Count == 0)
+                    return true;
+
+                List<StardewValley.Object> OriginalStock;
+
+                //if BFAV isn't installed/not enabled, grab the vanilla list
+                if (ModEntry.BFAV == null || !ModEntry.BFAV.IsEnabled())
                 {
-                    if (!ModEntry.ExcludeFromMarnie.Contains(animal.Name))
+                    OriginalStock = __result;
+                } else //otherwise use the BFAV list
+                {
+                    OriginalStock = ModEntry.BFAV.GetAnimalShopStock(Game1.getFarm());
+                }
+
+                //build a new list without the excluded animals
+                var newAnimalStock = new List<StardewValley.Object>();
+                foreach (var animal in OriginalStock)
+                {
+                    if (ModEntry.ExcludeFromMarnie.Contains(animal.Name))
+                    {
+                        ModEntry.monitor.Log($"{animal.Name} was removed from Marnie's stock.");   
+                    } else
                     {
                         newAnimalStock.Add(animal);
                     }
