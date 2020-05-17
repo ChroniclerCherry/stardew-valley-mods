7d5ef78bab959ccce2ae1c57fcc026ca958882e9
Cherry <yiminxuezheng@gmail.com>
2020-05-16 22:14:06 -0400

    Initial commit

---
+++ ShopEditor/ModEntry.cs
@@ -0,0 +1,13 @@
+﻿using StardewModdingAPI;
+using ShopTileFramework;
+
+namespace ShopEditor
+{
+    public class ModEntry : Mod
+    {
+        public override void Entry(IModHelper helper)
+        {
+            Monitor.Log("EEEEEEEEEEEEEEEEEEEEEEEEEEY", LogLevel.Error);
+        }
+    }
+}
