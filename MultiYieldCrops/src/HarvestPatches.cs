ffc951a453001df87c32289b690f5a3a33f4900b
Yiminxue Zheng <yiminxuezheng@gmail.com>
2020-07-15 20:27:03 -0400

    Fixed detection of non-regrowable crops

--- MultiYieldCrops/src/HarvestPatches.cs
+++ MultiYieldCrops/src/HarvestPatches.cs
@@ -1,11 +1,11 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System;
+using Microsoft.Xna.Framework;
 using StardewModdingAPI;
 using StardewValley;
 using StardewValley.Characters;
 using StardewValley.TerrainFeatures;
-using System;
 
-namespace MultiYieldCrop
+namespace MultiYieldCrops
 {
     class HarvestPatches
     {
@@ -21,29 +21,24 @@ namespace MultiYieldCrop
         public static void CropHarvest_prefix(Crop __instance,out int __state)
         {
             //dayOfCurrentPhase counts down how many days left before a reharvestable crop can be harvested again
-            __state = __instance.dayOfCurrentPhase.Get();
+            __state = __instance.dayOfCurrentPhase.Value;
         }
 
         public static void CropHarvest_postfix(int xTile, int yTile, HoeDirt soil, JunimoHarvester junimoHarvester,
             Crop __instance, bool __result, int __state)
         {
+            //__result is true is a crop was successfully harvested unless it's regrowable
+            if (!__result && __instance.regrowAfterHarvest.Value < 0) return;
 
-            //ignore if this is a forage crop or the crop is dead or not fully grown
-            if (__instance.forageCrop.Get() || __instance.dead.Get() || !__instance.fullyGrown.Get())
-                return;
-
-            if (__instance.regrowAfterHarvest.Get() > 0 &&__state > 1)
-                return;
-            
-            if (!__result && __instance.regrowAfterHarvest.Get() <= 0)
-                return;
+            //we reach this point if it's a regrowable crop and check its dayOfCurrentPhase from the prefix
+            if (__instance.regrowAfterHarvest.Value > 0 &&__state > 1) return;
 
             try
             {
-                int cropID = __instance.indexOfHarvest.Get();
-                string cropName = new StardewValley.Object(__instance.indexOfHarvest.Get(), 1, false).Name;
-                MultiYieldCrops.instance.SpawnHarvest(new Vector2(xTile,yTile),
-                    cropName, soil.fertilizer.Get(),junimoHarvester);
+                int cropId = __instance.indexOfHarvest.Value;
+                string cropName = new StardewValley.Object(cropId, 1, false).Name;
+                MultiYieldCrop.MultiYieldCrops.instance.SpawnHarvest(new Vector2(xTile,yTile),
+                    cropName, soil.fertilizer.Value,junimoHarvester);
 
             } catch (Exception ex)
             {
@@ -54,11 +49,12 @@ namespace MultiYieldCrop
         public static void BushPerformUseAction_postfix(Vector2 tileLocation, Bush __instance)
         {
             //not implemented yet
-            if ( __instance.inBloom(Game1.currentSeason, Game1.dayOfMonth) && __instance.size.Get() == Bush.greenTeaBush)
+            if ( __instance.inBloom(Game1.currentSeason, Game1.dayOfMonth) && __instance.size.Value == Bush.greenTeaBush)
             {
-                MultiYieldCrops.instance.SpawnHarvest(tileLocation, "Tea Leaves", HoeDirt.noFertilizer);
+                MultiYieldCrop.MultiYieldCrops.instance.SpawnHarvest(tileLocation, "Tea Leaves", HoeDirt.noFertilizer);
             }
         }
 
     }
+
 }
