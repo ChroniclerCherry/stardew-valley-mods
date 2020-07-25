1929714f5c6581710e34fe2147f39856c131312f
Yiminxue Zheng <yiminxuezheng@gmail.com>
2020-07-25 19:19:23 -0400

    Initial commit for Expanded Preconditions Utility

---
+++ ExpandedPreconditionsUtility/ModEntry.cs
@@ -0,0 +1,20 @@
+﻿using StardewModdingAPI;
+
+namespace ExpandedPreconditionsUtility
+{
+    public class ModEntry : Mod
+    {
+        private IModHelper _helper;
+        private IMonitor _monitor;
+        public override void Entry(IModHelper helper)
+        {
+            _helper = Helper;
+            _monitor = Monitor;
+        }
+
+        public override object GetApi()
+        {
+            return new ExpandedPreconditionsUtility(_monitor,_helper);
+        }
+    }
+}
