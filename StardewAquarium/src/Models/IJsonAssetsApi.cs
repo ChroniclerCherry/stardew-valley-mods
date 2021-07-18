6c88123bae8aefa04b43942bedf6e2bcbf70e1d1
donko526 <66753661+donko526@users.noreply.github.com>
2021-07-17 20:55:35 -0500

    deleted the mods I accidently branched

--- StardewAquarium/src/Models/IJsonAssetsApi.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace StardewAquarium.Models
-{
-    public interface IJsonAssetsApi
-    {
-        int GetObjectId(string name);
-        int GetBigCraftableId(string name);
-        void LoadAssets(string path);
-    }
-}
