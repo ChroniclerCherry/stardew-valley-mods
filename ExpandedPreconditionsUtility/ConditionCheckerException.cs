b5dec0ccda217d754a1e97077d3e690cecedfa63
Ellen Jana Fawkesová <4710267+purrplingcat@users.noreply.github.com>
2021-09-04 19:30:23 +0200

    EPU: Add support for custom conditions (#42)
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * EPU: Add support for custom conditions
    
    Co-authored-by: Yiminxue Zheng <33401364+ChroniclerCherry@users.noreply.github.com>

---
+++ ExpandedPreconditionsUtility/ConditionCheckerException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace ExpandedPreconditionsUtility
+{
+    [Serializable]
+    public class ConditionCheckerException : Exception
+    {
+        internal ConditionCheckerException()
+        {
+        }
+
+        internal ConditionCheckerException(string message) : base(message)
+        {
+        }
+
+        internal ConditionCheckerException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
