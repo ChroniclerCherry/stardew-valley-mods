121d561880f8a0a398b1a01df0bce783f3647806
BSavage81 <beau.j.savage@gmail.com>
2024-12-16 15:55:19 -0600

    Add Slime Ball max code
    Adds code to increase the max slime balls per day, along with a GMCM menu.

--- ChangeSlimeHutchLimit/Framework/Config.cs
+++ ChangeSlimeHutchLimit/Framework/Config.cs
@@ -1,6 +1,9 @@
-namespace ChangeSlimeHutchLimit.Framework;
-
-public class Config
+namespace ChangeSlimeHutchLimit.Framework
 {
-    public int MaxSlimesInHutch { get; set; } = 20;
+    public class Config
+    {
+        public int MaxSlimesInHutch { get; set; } = 20; // Default maximum slimes
+        public bool EnableSlimeBallOverride { get; set; } = true; // Default to enabled
+        public int MaxDailySlimeBalls { get; set; } = 0; // Default to unlimited (0)
+    }
 }
